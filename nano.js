393e21ccabc75ae9d0d66b1ce1cf38a8cdaf37e2
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-01-27 12:04:50 +0900

    Added transitions for animations through JS

--- nano.js
+++ nano.js
@@ -1,1 +1,25 @@
 
+
+corona1 = document.getElementById('corona')
+
+function chg(element) {
+  element.style.width = "350px";
+  element.style.height = "350px";
+}
+
+function chg2(element) {
+  element.style.width = "300px";
+  element.style.height = "300px";
+}
+
+function bounce(element) {
+element.style.marginTop = "0px";
+
+}
+
+corona1.addEventListener('mouseover', function() {chg(corona1)});
+
+corona1.addEventListener('mouseout', function() {chg2(corona1)});
+
+
+  window.onload = function() {bounce(corona1)};
