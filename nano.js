066c276c385e0f6caa4fe1e2ff45bb30763f0070
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-01-28 01:47:21 +0900

    Update nano.js

--- nano.js
+++ nano.js
@@ -1,25 +1,51 @@
 
-
 corona1 = document.getElementById('corona')
+menu1= document.getElementById('menu1')
+menu2 = document.getElementById('menu2')
+
 
 function chg(element) {
-  element.style.width = "350px";
-  element.style.height = "350px";
+  element.style.width = "450px";
+  element.style.height = "450px";
+
+  navs = document.getElementsByClassName('nav')
+  for(i=0;i<navs.length;i++) {
+    navs[i].style.display = "block";
+  }
 }
 
 function chg2(element) {
   element.style.width = "300px";
   element.style.height = "300px";
+
+  navs = document.getElementsByClassName('nav')
+  for(i=0;i<navs.length;i++) {
+    navs[i].style.display = "none";
+  }
 }
 
-function bounce(element) {
+function imageInit(element) {
 element.style.marginTop = "0px";
-
 }
+
+
+function titleInit(element) {
+  element.style.marginRight = "0px";
+  }
+
+  function title2Init(element) {
+    element.style.marginLeft= "0px";
+    }
+
+
 
 corona1.addEventListener('mouseover', function() {chg(corona1)});
 
 corona1.addEventListener('mouseout', function() {chg2(corona1)});
 
 
-  window.onload = function() {bounce(corona1)};
+  window.onload = function() {
+    titleInit(menu1)
+    title2Init(menu2)
+    imageInit(corona1)
+  };
