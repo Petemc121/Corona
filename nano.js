361348208ffbc2f5a1623bc498e8bc14da065377
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-02-04 19:21:42 +0900

    Update nano.js

--- nano.js
+++ nano.js
@@ -152,7 +152,34 @@ news.addEventListener('click', function() {
    
 })
 
+function progressBar(progressVal,totalPercentageVal = 100) {
+  var strokeVal = (4.64 * 100) /  totalPercentageVal;
+var x = document.querySelector('.progress-circle-prog');
+  x.style.strokeDasharray = progressVal * (strokeVal) + ' 999';
+var el = document.querySelector('.progress-text'); 
+var from = $('.progress-text').data('progress');
+$('.progress-text').data('progress', progressVal);
+var start = new Date().getTime();
 
+setTimeout(function() {
+    var now = (new Date().getTime()) - start;
+    var progress = now / 700;
+    el.innerHTML = progressVal / totalPercentageVal * 100 + '%';
+    if (progress < 1) setTimeout(arguments.callee, 10);
+}, 10);
+
+}
+
+infectedStat = Number(document.getElementById('infectedStat').innerHTML)
+recoveredStat = Number(document.getElementById('recoveredStat').innerrHTML)
+deceasedStat = Number(document.getElementById('deceasedStat').innerHTML)
+
+
+console.log(infectedStat)
+console.log(document.getElementById('infectedStat').innerHTML)
+
+
+progressBar(infectedStat,200);
 
    
 
