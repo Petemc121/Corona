0dc3a6193952b2b916c1557096b790663f707029
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-02-20 00:20:41 +0900

    Update fetchAPI2.js

--- fetchAPI2.js
+++ fetchAPI2.js
@@ -16,9 +16,15 @@ fetch("https://api.nhs.uk/conditions/coronavirus-covid-19?url=corona app&modules
   symptoms = document.getElementById('symptoms');
   selfIsolation = document.getElementById('selfIsolation');
   treatments = document.getElementById('treatments');
+  selfCare = document.getElementById('SelfCare');
+  prevention = document.getElementById('prevention');
 
   overview.innerHTML = data.modules[0].description;
   symptoms.innerHTML = data.modules[1].description;
+  selfIsolation.innerHTML = data.modules[2].description;
+  treatments.innerHTML = data.modules[3].description;
+  selfCare.innerHTML = data.modules[4].description;
+  prevention.innerHTML = data.modules[6].description;
   
 
 })
